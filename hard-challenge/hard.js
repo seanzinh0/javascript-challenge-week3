bc7f88396aa40edbf8a53360ecd0105dc39b18fa
spichay <seanpichay1015@gmail.com>
2024-11-16 11:42:37 -0500

    refactor hard challenge to use closure instead of classes

--- hard-challenge/hard.js
+++ hard-challenge/hard.js
@@ -1,24 +1,28 @@
-class PII {
-    #name = null;
-    #ssn = null;
+function createPII(name, ssn) {
+    let privateName = name;
+    let privateSSN = ssn;
 
-    constructor(name, ssn) {
-        this.#name = name;
-        this.#ssn = ssn;
-    }
+    const PII= {
+        getName: function() {
+            return privateName;
+        },
+        getSSN: function() {
+            return "NO ACCESS";
+        }
+    };
 
-    getName(){
-        return this.#name;
-    }
-
-    getSSN(){
-        return this.#ssn;
-    }
+    return PII;
 }
 
-const user = new PII("sean", "222-22-2222");
+const user = createPII("sean", "222-22-2222");
 
 console.log(user.name);
 console.log(user.ssn);
 console.log(user.getName());
 console.log(user.getSSN());
+
+const patient2 = createPII("John", "123-45-6789");
+console.log(patient2.name);
+console.log(patient2.ssn);
+console.log(patient2.getName());
+console.log(patient2.getSSN());
